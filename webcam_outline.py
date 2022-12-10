ad61c427d125575a53f6bc97418106b826474a63
Pekeps <118191015+Pekeps@users.noreply.github.com>
2022-12-10 20:56:12 +0200

    Update webcam_outline.py

--- webcam_outline.py
+++ webcam_outline.py
@@ -15,30 +15,10 @@ while True:
     can = cv2.Canny(gb, 127, 31)
     cv2.imshow('Canny edge detection', can)
     cv2.imshow("Webcam", gb)
-
-
     if cv2.waitKey(1) == 32:
         timestr = t.strftime("%Y%m%d-%H%M%S")
         cv2.imwrite("frame%s.jpg" % timestr, can)
-
-
-
-
-
-
-
-
     if cv2.waitKey(5) & 0xFF == 27:
         break
 cap.release()
 cv2.destroyAllWindows()
-
-def generate_ascii_letters():
-    images = []
-    #letters = "# $%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-    letters = " \\ '(),-./:;[]_`{|}~"
-    for letter in letters:
-        img = np.zeros((12, 16), np.uint8)
-        img = cv2.putText(img, letter, (0, 11), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255)
-        images.append(img)
-    return np.stack(images)
